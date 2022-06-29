4a868aa665a64e16b749c0277c1b7b0675e62ba4
cwhite098 <gd19031@bristol.ac.uk>
2022-06-29 16:05:46 +0100

    modified network structure, 1 more conv layer

--- neural_net/model.py
+++ neural_net/model.py
@@ -1,7 +1,7 @@
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD, Adam
 
 def create_network(input_height, input_width, num_outputs):
 
@@ -16,10 +16,11 @@ def create_network(input_height, input_width, num_outputs):
     model.add(Conv2D(filters=64, kernel_size=(5,5), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
 
+    '''
     # 3rd convolution
     model.add(Conv2D(filters=128, kernel_size=(5,5), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-
+    
     # 4th convolution
     model.add(Conv2D(filters=128, kernel_size=(5,5), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
@@ -27,16 +28,18 @@ def create_network(input_height, input_width, num_outputs):
     # 5th convolution
     model.add(Conv2D(filters=256, kernel_size=(5,5), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    '''
 
     # Flatten the convolved images
     model.add(Flatten())
 
     model.add(Dense(128, activation='relu'))
+    model.add(Dense(128, activation='relu'))
     model.add(Dense(64, activation='relu'))
 
-    model.add(Dense(num_outputs))
+    model.add(Dense(num_outputs, activation='linear'))
 
-    model.compile(loss='mean_squared_error', 
-                    optimizer=SGD(learning_rate=0.0001), metrics=['accuracy'])
+    model.compile(loss='mean_absolute_error', 
+                    optimizer=Adam(learning_rate=0.01), metrics=['accuracy'])
 
     return model
