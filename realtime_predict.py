124c91597a8d38eee7da5e1d9edc84050f977955
cwhite098 <gd19031@bristol.ac.uk>
2022-07-18 16:14:20 +0100

    added thread for loading the NNs and getting the predictions

--- realtime_predict.py
+++ realtime_predict.py
@@ -26,6 +26,7 @@ from markers.camera_calibration.calibrate_camera import calibrate
 from markers.detect_markers import find_markers
 from collect_data import process_sensor_frame, camera_thread
 import settings
+from neural_net.model import PoseNet
 
 
 
@@ -36,8 +37,61 @@ import settings
 # plan out final output frame
 # include kill switch
 
+class predict_thread(threading.Thread):
+    def __init__(self, sensor_net_path, object_net_path):
+        threading.Thread.__init__(self)
+        self.sensor_net_path = sensor_net_path
+        self.object_net_path = object_net_path
 
-class predict_display_thread(threading.Thread):
+    def run(self):
+
+        self.load_nets()
+        time.sleep(5)
+
+        while True:
+            l_sensor = settings.frame_buffer['Sensor1']
+            r_sensor = settings.frame_buffer['Sensor2']
+            input_frame = np.vstack((l_sensor, r_sensor)).reshape((1, 270,240,1))
+            sensor_angles = self.sensor_net.predict(input_frame)
+            object_angles = self.object_net.predict(input_frame)
+            settings.prediction_buffer['sensor'] = sensor_angles
+            settings.prediction_buffer['object'] = object_angles
+
+    def load_nets(self):
+        # Init 2 posenet classses with any args
+        self.object_net = PoseNet(  option = 'object',
+                    conv_activation = 'elu',
+                    dropout_rate = 0.001,
+                    l1_rate = 0.0001,
+                    l2_rate = 0.01,
+                    learning_rate = 0.00001,
+                    decay_rate = 0.000001,
+                    dense_width = 16,
+                    loss_func = 'mse',
+                    batch_bool = False,
+                    N_convs = 4,
+                    N_filters = 512
+                     )
+        self.sensor_net = PoseNet(  option = 'sensor',
+                    conv_activation = 'elu',
+                    dropout_rate = 0.001,
+                    l1_rate = 0.0001,
+                    l2_rate = 0.01,
+                    learning_rate = 0.00001,
+                    decay_rate = 0.000001,
+                    dense_width = 16,
+                    loss_func = 'mse',
+                    batch_bool = False,
+                    N_convs = 4,
+                    N_filters = 512
+                     )
+
+        self.object_net.load_net(self.object_net_path)
+        self.sensor_net.load_net(self.sensor_net_path)
+        
+
+
+class display_thread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
 
@@ -45,7 +99,8 @@ class predict_display_thread(threading.Thread):
         self.make_frame()
 
     def make_frame(self):
-        time.sleep(5)
+        print('[INFO] Starting, Please Wait...')
+        time.sleep(10)
         while True:
             l_sensor = settings.frame_buffer['Sensor1']
             r_sensor = settings.frame_buffer['Sensor2']
@@ -62,11 +117,22 @@ class predict_display_thread(threading.Thread):
 
             full = np.vstack((sensors_ext, blank))
 
-            cv.imshow('Realtime Prediction', full)
+            # Get the true rotations from the markers
+            left_rvec = settings.rvec_buffer['left'][1:]
+            right_rvec = settings.rvec_buffer['right'][1:]
+            object_rvec = settings.rvec_buffer['object']
 
-            # add code here to add the rotation vectors to the bottom of this frame
-            # add code here to use the sensor frames to predict the rotation vectors
-            # add the predicted rotations to the frame as well for comparison
+            # Get the predicted rotations from the NNs
+            try:
+                sensor_prediction = settings.prediction_buffer['sensor']
+                object_prediction = settings.prediction_buffer['object']
+            except KeyError:
+                print('No predictions!!')
+
+            # Display the rotations here
+
+
+            cv.imshow('Realtime Prediction', full)
 
             key = cv.waitKey(20)
             if key == 27:
@@ -75,6 +141,10 @@ class predict_display_thread(threading.Thread):
 
 
 class kill_switch(threading.Thread):
+    '''
+    Thread that runs in the background and will kill all threads when the
+    right key is pressed.
+    '''
     def __init__(self):
         threading.Thread.__init__(self)
 
@@ -109,13 +179,17 @@ def main():
     sensor2 = camera_thread('Sensor2', 1, (1920,1080), mode, process_sensor_frame, False, res=(240,135), crop = [300, 0, 1600, 1080], threshold = (59, -6))
     external = camera_thread('external', 2, (1920,1080), mode, find_markers, False, cam_mat=cam_mat, dist_mat=dist_mat)
 
-    predict_display = predict_display_thread()
+    sensor_path = 'combined_Tue_Jul__5_19-06-13_2022'
+    object_path = 'object_Sat_Jul_16_18-10-46_2022'
+    predict = predict_thread('neural_net/saved_nets/'+sensor_path, 'neural_net/saved_nets/'+object_path)
+    display = display_thread()
     x_switch = kill_switch()
 
     sensor1.start()
     sensor2.start()
     external.start()
-    predict_display.start()
+    predict.start()
+    display.start()
     x_switch.start()
 
     return 0
