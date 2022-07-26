34ed25ddefa9e3daee826e46935a8175352a4041
cwhite098 <gd19031@bristol.ac.uk>
2022-07-26 12:50:51 +0100

    training more nets

--- neural_net/train.py
+++ neural_net/train.py
@@ -12,26 +12,26 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    shapes = ['pose_dodec_so', 'cube', 'cylinder']
+    shapes = ['pose_dodec_so']
     path = 'C:/Users/chris/OneDrive/Uni/Summer_Research_Internship/Project/TacTip_Orientation/data'
     
     scale = False
     val = True
-    outlier_bool = True
-    sensor = 'combined'
+    outlier_bool = False
+    sensor = 'relative'
 
     CNN = PoseNet(  option = sensor,
                     conv_activation = 'elu',
-                    dropout_rate = 0.0001,
+                    dropout_rate = 0.001,
                     l1_rate = 0.0001,
-                    l2_rate = 0.001,
+                    l2_rate = 0.01,
                     learning_rate = 0.00001,
                     decay_rate = 0.000001,
-                    dense_width = 8,
+                    dense_width = 16,
                     loss_func = 'mse',
-                    batch_bool = True,
-                    N_convs = 5,
-                    N_filters = 128
+                    batch_bool = False,
+                    N_convs = 4,
+                    N_filters = 512
                      )
 
     x_train, x_test, x_val, y_train, y_test, y_val, test_scaler = CNN.read_data(shapes, path,
@@ -45,6 +45,7 @@ def main():
     p.plot_angle_distribution(y_train, y_test, sensor, y_val=y_val)
 
     CNN.create_network(x_train[0].shape[0], 240, y_train.shape[1]) # create the NN
+    #CNN.create_network_di(135, 240, y_train.shape[1])
     CNN.summary()
     CNN.fit(x_train, y_train, epochs=500, batch_size=8, x_val=x_val, y_val=y_val) # train the NN
     CNN.evaluate(x_test, y_test) # evaluate the NN
@@ -52,9 +53,6 @@ def main():
     CNN.plot_learning_curves()
 
     loss = CNN.loss
-    if scale:
-        loss_unscaled = test_scaler.inverse_transform(np.array([loss,loss,loss,loss]))
-        print(loss_unscaled)
 
     return 0
 
