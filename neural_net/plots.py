7f1c4582cce33d5f0271298cd060f3a30c750c9a
cwhite098 <gd19031@bristol.ac.uk>
2022-07-26 12:49:42 +0100

    added line plots and modified main to generate predictions and plot them

--- neural_net/plots.py
+++ neural_net/plots.py
@@ -1,9 +1,25 @@
+import os
+import json
 import matplotlib.pyplot as plt
 import numpy as np
 import model
+import matplotlib
+
+
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 13}
+
+matplotlib.rc('font', **font)
+
+matplotlib.rc('xtick', labelsize=10) 
+matplotlib.rc('ytick', labelsize=12)
 
 
 def plot_angle_distribution(y_train, y_test, sensor, y_val=None):
+
+    y_train = rad_to_degree(y_train)
+    y_test = rad_to_degree(y_test)
 
 
     if sensor == 'combined':
@@ -13,7 +29,7 @@ def plot_angle_distribution(y_train, y_test, sensor, y_val=None):
         plt.scatter(y_test[:,0], y_test[:,1], label='Test')
         if isinstance(y_val, np.ndarray):
             plt.scatter(y_val[:,0], y_val[:,1], label='Val')
-        plt.legend(), plt.xlabel('Psi1'), plt.ylabel('Phi1')
+        plt.legend(), plt.xlabel('Yaw'), plt.ylabel('Pitch')
         plt.title('Sensor 1 Angles')
 
         plt.subplot(1,2,2)
@@ -21,7 +37,7 @@ def plot_angle_distribution(y_train, y_test, sensor, y_val=None):
         plt.scatter(y_test[:,2], y_test[:,3], label='Test')
         if isinstance(y_val, np.ndarray):
             plt.scatter(y_val[:,2], y_val[:,3], label='Val')
-        plt.legend(), plt.xlabel('Psi2'), plt.ylabel('Phi2')
+        plt.legend(), plt.xlabel('Yaw'), plt.ylabel('Pitch')
         plt.title('Sensor 2 Angles')
 
     elif sensor == 'object':
@@ -30,16 +46,16 @@ def plot_angle_distribution(y_train, y_test, sensor, y_val=None):
         plt.scatter(y_test[:,0], y_test[:,1], label='Test')
         if isinstance(y_val, np.ndarray):
             plt.scatter(y_val[:,0], y_val[:,1], label='Val')
-        plt.legend(), plt.xlabel('F/B'), plt.ylabel('S/S')
-        plt.title('Sensor 1 Angles')
+        plt.legend(), plt.xlabel('Object Roll'), plt.ylabel('Object Yaw')
+        plt.title('Object Pitch vs Roll')
 
         plt.subplot(1,2,2)
         plt.scatter(y_train[:,0], y_train[:,2], label='Train')
         plt.scatter(y_test[:,0], y_test[:,2], label='Test')
         if isinstance(y_val, np.ndarray):
             plt.scatter(y_val[:,0], y_val[:,2], label='Val')
-        plt.legend(), plt.xlabel('F/B'), plt.ylabel('U/D')
-        plt.title('Sensor 2 Angles')
+        plt.legend(), plt.xlabel('Object Roll'), plt.ylabel('Object Pitch')
+        plt.title('Object Yaw vs Roll')
 
 
     else:
@@ -74,20 +90,93 @@ def plot_angle_hists(y_train, y_test, y_val=None):
 
 def plot_prediction_line(true, pred, no_angles):
 
+    true = (true/(2*np.pi))*360
+    pred = (pred/(2*np.pi))*360
+
     for i in range(no_angles):
         plt.subplot(1,no_angles,i+1)
-        plt.plot([np.min(true), np.max(true)],[np.min(true), np.max(true)])
-        plt.scatter(true[:,i], pred[:,i])
+        plt.tight_layout()
+        
+        plt.plot([np.min(true), np.max(true)],[np.min(true), np.max(true)], c='r', linewidth=2, zorder=1)
+        plt.scatter(true[:,i], pred[:,i], marker='*', s=10, zorder=2)
         plt.xlabel('True Angles '+str(i)), plt.ylabel('Predicted Angle '+str(i))
+        plt.grid()
     plt.show()
 
+def rad_to_degree(array):
+    array = (array/(2*np.pi))*360
+    return array
+
+def plot_error_distributions():
+
+    '''
+    Reads the angles from the 'error' folder and plots them as histograms to show the variation
+    that occurs when using the aruco markers for pose estimation.
+    '''
+    path = 'C:/Users/chris/OneDrive/Uni/Summer_Research_Internship/Project/TacTip_Orientation/data/error/'
+    snapshots = os.listdir(path)
+    rots = ['Roll', 'Pitch', 'Yaw']
+    left_list = []
+    right_list = []
+    object_list = []
+    ref_list = []
+    for example in snapshots:
+        # Read the orientation data
+        with open(path+example+'/rvec.json', 'r') as f:
+            rvec_dict = json.load(f)
+            left_list.append(rvec_dict['left'])
+            right_list.append(rvec_dict['right'])
+            object_list.append(rvec_dict['object'])
+            ref_list.append(rvec_dict['reference'])
+    left_list = rad_to_degree(np.array(left_list))
+    right_list = rad_to_degree(np.array(right_list))
+    object_list = rad_to_degree(np.array(object_list))
+    ref_list = rad_to_degree(np.array(ref_list))
+
+    # Add some code to get means and std of each of the angles
+    # Display this information in the titles of the plots
+    list_of_lists = [left_list, right_list, object_list, ref_list]
+    stats = []
+    for i in range(4):
+        list = list_of_lists[i]
+        for i in range(3):
+            mean = np.around(np.mean(list[:,i]),3)
+            std = np.around(np.std(list[:,i]),3)
+            stats.append([mean, std])
+
+    bin_no = 20
+
+    for i in range(3):
+        plt.subplot(4,3,i+1)
+        plt.hist(left_list[:,i], bins=bin_no, edgecolor='black', linewidth=1.2)
+        plt.title('Left '+rots[i]+' '+str(stats[i]))
+    for i in range(3):
+        plt.subplot(4,3,i+4)
+        plt.hist(right_list[:,i], bins=bin_no, edgecolor='black', linewidth=1.2)
+        plt.title('Right '+rots[i]+' '+str(stats[i+3]))
+    for i in range(3):
+        plt.subplot(4,3,i+7)
+        plt.hist(object_list[:,i], bins=bin_no, edgecolor='black', linewidth=1.2)
+        plt.title('Object '+rots[i]+' '+str(stats[i+6]))
+    for i in range(3):
+        plt.subplot(4,3,i+10)
+        plt.hist(ref_list[:,i], bins=bin_no, edgecolor='black', linewidth=1.2)
+        plt.title('Reference '+rots[i]+' '+str(stats[i+9]))
+
+    plt.tight_layout()
+    plt.show()
+
+    return 0
 
 
 def main():
 
-    net_path = 'neural_net/saved_nets/combined_Mon_Jul_18_16-20-32_2022'
 
-    sensor_net = model.PoseNet(  option = 'sensor',
+    plot_error_distributions()
+    
+    net_path = 'saved_nets/object_Mon_Jul_25_17-05-24_2022'
+
+    sensor_net = model.PoseNet(  option = 'object',
                     conv_activation = 'elu',
                     dropout_rate = 0.001,
                     l1_rate = 0.0001,
@@ -104,18 +193,20 @@ def main():
     sensor_net.load_net(net_path)
 
     path = 'C:/Users/chris/OneDrive/Uni/Summer_Research_Internship/Project/TacTip_Orientation/data'
-    x_train, x_test, x_val, y_train, y_test, y_val, test_scaler = sensor_net.read_data(['pose_dodec_so', 'cube', 'cylinder'], path,
-                                                        option = 'combined',
+    x_train, x_test, x_val, y_train, y_test, y_val, test_scaler = sensor_net.read_data(['pose_dodec_so', 'pose_dodec'], path,
+                                                        option = 'object',
                                                         val = True, 
                                                         combine_shapes = True,
                                                         scale_data = False,
-                                                        outlier_bool = True,
+                                                        outlier_bool = False,
                                                         binarise=True)
+
+    #plot_angle_distribution(y_train, y_test, 'combined', y_val=None)
 
     pred = sensor_net.predict(x_test)
 
-    plot_prediction_line(y_test, pred, 4)
-
+    plot_prediction_line(y_test, pred, 3)
+    
     return 0
 
 
